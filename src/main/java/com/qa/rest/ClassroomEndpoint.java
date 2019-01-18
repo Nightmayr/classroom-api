a670f044de98a59be45169697221d25be792fa6e
Umayr <umayr23@gmail.com>
2019-01-18 12:21:45 +0000

    Added update and delete endpoints for Classroom

--- src/main/java/com/qa/rest/ClassroomEndpoint.java
+++ src/main/java/com/qa/rest/ClassroomEndpoint.java
@@ -1,9 +1,12 @@
 package com.qa.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.qa.business.service.ClassroomService;
@@ -28,5 +31,22 @@ public class ClassroomEndpoint {
 		return service.addClassroom(classroom);
 	}
 	
+	@Path("/updateClassroom/{id}")
+	@PUT
+	@Produces({"application/json"})
+	public String upddateClassroom(@PathParam("id") Long id, String classroom) {
+		return service.updateClassroom(id, classroom);
+	}
+	
+	@Path("/deleteClassroom/{id}")
+	@DELETE
+	@Produces({"application/json"})
+	public String deleteClassroom(@PathParam("id") Long id) {
+		return service.deleteClassroom(id);
+	}
+	
+	public void setService(ClassroomService service) {
+		this.service = service;
+	}
 	
 }
