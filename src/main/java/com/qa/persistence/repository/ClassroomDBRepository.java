133e6a907085583d92e9904915b8fc5a62302d8c
Umayr <umayr23@gmail.com>
2019-01-18 10:57:39 +0000

    Added interface methods to ClassroomDBRepo

--- src/main/java/com/qa/persistence/repository/ClassroomDBRepository.java
+++ src/main/java/com/qa/persistence/repository/ClassroomDBRepository.java
@@ -1,5 +1,31 @@
 package com.qa.persistence.repository;
 
-public class ClassroomDBRepository {
+import javax.enterprise.inject.Default;
+import javax.transaction.Transactional;
+
+	@Transactional(SUPPORTS)
+	@Default
+	public class ClassroomDBRepository implements ClassroomRepository{
+
+		public String getAllClassrooms() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public String addClassroom(String classroom) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public String deleteClassroom(Long id) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public String updateClassroom(Long id, String classroom) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
 
 }
