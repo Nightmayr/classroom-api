0ef289703313e84cce619ef8a3de445631347f07
Umayr <umayr23@gmail.com>
2019-01-18 12:03:11 +0000

    Mirrored ClassroomServiceImp functionality in Trainee equivalent

--- src/main/java/com/qa/business/service/TraineeServiceImplementation.java
+++ src/main/java/com/qa/business/service/TraineeServiceImplementation.java
@@ -1,5 +1,32 @@
 package com.qa.business.service;
 
-public class TraineeServiceImplementation {
+import javax.inject.Inject;
+
+import com.qa.persistence.domain.Trainee;
+import com.qa.persistence.repository.TraineeRepository;
+import com.qa.util.JSONUtil;
+
+public class TraineeServiceImplementation implements TraineeService{
+	@Inject
+	private TraineeRepository repo;
+	@Inject
+	private JSONUtil util;
 	
+	
+	public String getAllTrainees() {
+		return repo.getAllTrainees();
+	}
+
+	public String addTrainee(String trainee) {
+		Trainee checkTrainee = util.getObjectForJSON(trainee, Trainee.class);
+		return repo.addTrainee(trainee);
+	}
+
+	public String deleteTrainee(Long id) {
+		return repo.deleteTrainee(id);
+	}
+
+	public String updateTrainee(Long id, String trainee) {
+		return repo.updateTrainee(id, trainee);
+	}
 }
