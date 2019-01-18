b905ccc4d6de98939ade9fe5f0968020eb59af1a
Umayr <umayr23@gmail.com>
2019-01-18 10:37:02 +0000

    Created first version of Classroom entity

--- src/main/java/com/qa/persistence/domain/Classroom.java
+++ src/main/java/com/qa/persistence/domain/Classroom.java
@@ -1,8 +1,36 @@
 package com.qa.persistence.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Classroom {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	private String trainer;
+//	Trainees entity yet to be implemented
+//	private Trainee[] trainees;
+	
+	public Classroom() {
+		
+	}
+	
+	public Classroom(String trainer) {
+		this.setTrainer(trainer);
+	}
 
+	public String getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(String trainer) {
+		this.trainer = trainer;
+	}
+	
+	
+	
 }
