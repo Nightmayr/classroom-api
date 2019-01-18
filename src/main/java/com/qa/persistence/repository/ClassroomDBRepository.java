7847588c7b9365167f531eb550f1a53cd8f27c3f
Umayr <umayr23@gmail.com>
2019-01-18 11:06:29 +0000

    Added implementation for getAllClassrooms method

--- src/main/java/com/qa/persistence/repository/ClassroomDBRepository.java
+++ src/main/java/com/qa/persistence/repository/ClassroomDBRepository.java
@@ -1,11 +1,18 @@
 package com.qa.persistence.repository;
 
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+import java.util.Collection;
+
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 
+import com.qa.persistence.domain.Classroom;
 import com.qa.util.JSONUtil;
 
 	@Transactional(SUPPORTS)
@@ -15,13 +22,13 @@ import com.qa.util.JSONUtil;
 		@PersistenceContext(unitName = "primary")
 		private EntityManager manager;
 		
-	
 		@Inject
 		private JSONUtil util;
 		
 		public String getAllClassrooms() {
-			// TODO Auto-generated method stub
-			return null;
+			Query query = manager.createQuery("SELECT c FROM Classroom c");
+			Collection<Classroom> classrooms = (Collection<Classroom>query.getResultList());
+			return util.getJSONForObject(classrooms);
 		}
 
 		public String addClassroom(String classroom) {
