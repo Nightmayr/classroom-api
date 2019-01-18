478a246c5df8df6738d5b962649de408580fc49d
Umayr <umayr23@gmail.com>
2019-01-18 10:40:12 +0000

    Created Trainee class, began creation of Entity

---
+++ src/main/java/com/qa/persistence/domain/Trainee.java
@@ -0,0 +1,25 @@
+package com.qa.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Trainee {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	private String traineeName;
+	
+	
+	public String getTraineeName() {
+		return traineeName;
+	}
+	public void setTraineeName(String traineeName) {
+		this.traineeName = traineeName;
+	}
+	
+	
+}
