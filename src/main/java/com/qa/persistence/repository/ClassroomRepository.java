b0213c2310ab10da270a64a6827882d1d0ce43d9
Umayr <umayr23@gmail.com>
2019-01-18 10:54:25 +0000

    Added CRUD methods to ClassroomRepo Interface

--- src/main/java/com/qa/persistence/repository/ClassroomRepository.java
+++ src/main/java/com/qa/persistence/repository/ClassroomRepository.java
@@ -1,5 +1,10 @@
 package com.qa.persistence.repository;
 
-public class ClassroomRepository {
+public interface ClassroomRepository {
+	
+	String getAllClassrooms();
+	String addClassroom(String classroom);
+	String deleteClassroom(Long id);
+	String updateClassroom(Long id, String classroom);
 
 }
